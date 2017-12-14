8e6bdf2a73e8e0c1f0173dc9759f6f8de65e4a2c
moTzxx <JingYin007@users.noreply.github.com>
2017-12-14 16:51:08 +0800

    ok
    修改 优化

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -23,8 +23,8 @@ class TestController extends Controller
      * 发送邮件
      */
     public function mail(){
-        $tag = $this->sendText();
-        //$tag = $this->sendHtml();
+        //$tag = $this->sendText();
+        $tag = $this->sendHtml();
         //TODO  $tag 判断发送是否成功，进行后续代码开发
         return view('mail',['title' => '你若盛开，清风自来','author' => '木心']);
     }
@@ -35,15 +35,15 @@ class TestController extends Controller
     public function sendText(){
         //此处为文本内容
         $content = '从前的日色变得慢 车 马 邮件 都慢';
+        $email = [
+            'subject' => '这是邮件主题，希望您能支持！',//邮件主题
+            'to' => '930959695@qq.com',//邮件接收地址
+        ];
         $tag = $this->mailer
             ->raw($content,
-                function ($message){
-                    $emailData = [
-                        'subject' => '这是邮件主题，希望您能支持！',//邮件主题
-                        'to' => '930959695@qq.com',//邮件接收地址
-                    ];
-                    $message->subject($emailData['subject']);
-                    $message->to($emailData['to']);
+                function ($message)use ($email){
+                    $message->subject($email['subject']);
+                    $message->to($email['to']);
                 });
         return $tag;
     }
@@ -52,15 +52,15 @@ class TestController extends Controller
      */
     public function sendHtml(){
         $data = ['title' => '你若盛开，清风自来','author' => '木心'];
+        $email = [
+            'subject' => 'Hello My Dear,let`s go！',//邮件主题
+            'to' => '930959695@qq.com',//邮件接收地址
+        ];
         $tag = $this->mailer
             ->send('mail',$data,
-                function ($message){
-                    $emailData = [
-                        'subject' => 'Hello My Dear,let`s go！',//邮件主题
-                        'to' => '930959695@qq.com',//邮件接收地址
-                    ];
-                    $message->subject($emailData['subject']);
-                    $message->to($emailData['to']);
+                function ($message) use ($email){
+                    $message->subject($email['subject']);
+                    $message->to($email['to']);
                 });
         return $tag;
     }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -241,6 +241,10 @@ class ComposerStaticInit9ca7a31324ff1aa4aa80f092f9beb198
             array (
                 0 => __DIR__ . '/..' . '/mockery/mockery/library',
             ),
+            'Michelf' => 
+            array (
+                0 => __DIR__ . '/..' . '/michelf/php-markdown',
+            ),
         ),
         'J' => 
         array (
@@ -1433,6 +1437,9 @@ class ComposerStaticInit9ca7a31324ff1aa4aa80f092f9beb198
         'League\\Flysystem\\Util\\ContentListingFormatter' => __DIR__ . '/..' . '/league/flysystem/src/Util/ContentListingFormatter.php',
         'League\\Flysystem\\Util\\MimeType' => __DIR__ . '/..' . '/league/flysystem/src/Util/MimeType.php',
         'League\\Flysystem\\Util\\StreamHasher' => __DIR__ . '/..' . '/league/flysystem/src/Util/StreamHasher.php',
+        'Michelf\\Markdown' => __DIR__ . '/..' . '/michelf/php-markdown/Michelf/Markdown.php',
+        'Michelf\\MarkdownExtra' => __DIR__ . '/..' . '/michelf/php-markdown/Michelf/MarkdownExtra.php',
+        'Michelf\\MarkdownInterface' => __DIR__ . '/..' . '/michelf/php-markdown/Michelf/MarkdownInterface.php',
         'Mockery' => __DIR__ . '/..' . '/mockery/mockery/library/Mockery.php',
         'Mockery\\Adapter\\Phpunit\\MockeryPHPUnitIntegration' => __DIR__ . '/..' . '/mockery/mockery/library/Mockery/Adapter/Phpunit/MockeryPHPUnitIntegration.php',
         'Mockery\\Adapter\\Phpunit\\MockeryTestCase' => __DIR__ . '/..' . '/mockery/mockery/library/Mockery/Adapter/Phpunit/MockeryTestCase.php',
